dfeaf77f929b27d5d8f977745f8cb48f041c0b44
Filip Oščádal <f@mxd.cz>
2020-08-21 17:41:55 +0200

    documentation

---
+++ make_docs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# English
+cd ./docs/en/
+
+for i in *.adoc
+do
+  docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf "$i"
+done
+
+mv *.pdf ../../PDF/en/
+cd ../..
+
+
+# Czech
+cd ./docs/cs/
+
+for i in *.adoc
+do
+  docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf "$i"
+done
+
+mv *.pdf ../../PDF/cs/
+cd ../..
