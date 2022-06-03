215291fa8be32b2859b3e3ef4a6e58c42a46a09c
Filip Oščádal <f@mxd.cz>
2022-06-03 22:16:24 +0200

    minor update

--- bin/test.sh
+++ bin/test.sh
@@ -6,7 +6,7 @@ ABSDIR=$(dirname $ABSPATH)
 dir="$(dirname "$0")"
 . "$dir/_includes.sh"
 
-command -v red >/dev/null 2>&1 || fail "Docker is NOT installed!"
+command -v red >/dev/null 2>&1 || fail "Red is NOT installed!"
 
 info "Red version: $(red --version)"
 info "Red 'Hello World' example: $(red hello.red)"
